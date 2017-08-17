66c9323573d9c1abef8c27b0372e4d0f585f2760
Matthew Pickering <matthewtpickering@gmail.com>
2017-08-17 14:25:01 +0000

    kythe init at 0.0.26

---
+++ pkgs/development/tools/kythe/default.nix
@@ -0,0 +1,46 @@
+{ stdenv, binutils , fetchurl, glibc }:
+
+stdenv.mkDerivation rec {
+  version = "0.0.26";
+  name = "kythe-${version}";
+
+  src = fetchurl {
+    url = "https://github.com/google/kythe/releases/download/v0.0.26/kythe-v0.0.26.tar.gz";
+    sha256 = "0dij913fjymrbdlfjnr65ib90x5xd3smia3bh83q9prh7sfi5h07";
+  };
+
+  buildInputs =
+    [ binutils ];
+
+  doCheck = false;
+
+  buildPhase = ''
+  '';
+
+  testPhase = ''
+  '';
+
+  installPhase = ''
+    for exe in tools/* do
+      if [[-x "tools/$exe" ]] then
+        patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" tools/$exe
+        patchelf --set-rpath "${stdenv.cc.cc.lib}/lib64" tools/$exe
+    done
+    cp -R ./ $out
+  '';
+
+  meta = with stdenv.lib; {
+    description = "kythe";
+    longDescription = ''
+    The Kythe project was founded to provide and support tools and standards
+      that encourage interoperability among programs that manipulate source
+      code. At a high level, the main goal of Kythe is to provide a standard,
+      language-agnostic interchange mechanism, allowing tools that operate on
+      source code — including build systems, compilers, interpreters, static
+      analyses, editors, code-review applications, and more — to share
+      information with each other smoothly.  '';
+    homepage = https://kythe.io/;
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.all;
+  };
+}
