6c175063ddf3932582985b8a1e75613ae6505fb4
Matthew Pickering <matthewtpickering@gmail.com>
2018-03-20 18:41:37 +0000

    Build qscintilla on darwin

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14054,7 +14054,7 @@ in {
         description = "A Python binding to QScintilla, Qt based text editing control";
         license = licenses.lgpl21Plus;
         maintainers = with maintainers; [ danbst ];
-        platforms = platforms.linux;
+        platforms = platforms.unix;
       };
     });
 
